73095400324a97b2e1615ea823951e99aa0e3e46
Priyanshu Agrawal <agpri123@gmail.com>
2022-10-05 22:35:05 -0400

    added chess background and theme

--- src/assets_data/themes_data.hpp
+++ src/assets_data/themes_data.hpp
@@ -5,5 +5,9 @@
 namespace pico_trivia
 {
 	pico_trivia::Theme bamboo_theme(&pico_trivia::bamboo_background, {0, 0, 0});
-	pico_trivia::Theme tech_theme(&pico_trivia::pcb_trace_background, {255, 255, 255});
+	//pico_trivia::Theme tech_theme(&pico_trivia::pcb_trace_background, {255, 255, 255});
+	// temporary see backgrounds data file
+	pico_trivia::Theme tech_theme(&pico_trivia::chess_background, {255, 255, 255});
+	pico_trivia::Theme chess_theme(&pico_trivia::chess_background, {255, 255, 255});
+
 }
--- src/assets_data/trivia_questions_data.hpp
+++ src/assets_data/trivia_questions_data.hpp
@@ -7,8 +7,9 @@ namespace pico_trivia {
 	// This question appears normal to users, but indicates to me that new trivia questions need to be added
 	TriviaQuestion ERROR_QUESTION = TriviaQuestion("In which year was the first iPhone released?", "2007", {"2005", "2009", "2010"}, &tech_theme);
 	
-	std::array<TriviaQuestion, 1> trivia_questions = {
-		TriviaQuestion("How many bones do sharks have in their bodies?", "0", {"235", "206", "1095"}, &bamboo_theme)
+	std::array<TriviaQuestion, 2> trivia_questions = {
+		TriviaQuestion("How many bones do sharks have in their bodies?", "0", {"235", "206", "1095"}, &bamboo_theme),
+		TriviaQuestion("Which of the following is not a real chess opening?", "Black Tiger Attack", {"Bongcloud Attack", "Pterodactyl Defense", "Frankenstein-Dracula Variation"}, &chess_theme)
 	};
 	
 	TriviaQuestion get_todays_question(int daynumber)
