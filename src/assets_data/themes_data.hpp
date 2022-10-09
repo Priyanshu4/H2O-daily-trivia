fc67a552d5ed27efa9af7acdf658bc0aabf32d89
Priyanshu Agrawal <agpri123@gmail.com>
2022-10-09 18:48:04 -0400

    added question marks background

--- src/assets_data/themes_data.hpp
+++ src/assets_data/themes_data.hpp
@@ -5,6 +5,7 @@
 namespace pico_trivia
 {
 	pico_trivia::Theme H2O_theme(&pico_trivia::H2O_molecules_background, {32, 255, 0});
+	pico_trivia::Theme generic_theme(&pico_trivia::question_marks_background, {255, 255, 255});
 	pico_trivia::Theme bamboo_theme(&pico_trivia::bamboo_background, {0, 0, 0});
 	pico_trivia::Theme tech_theme(&pico_trivia::pcb_trace_background, {255, 255, 255});
 	pico_trivia::Theme chess_theme(&pico_trivia::chess_background, {255, 255, 255});
